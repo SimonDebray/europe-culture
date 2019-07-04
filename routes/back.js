117e5891a795f81aae7cb6867a167d48238cc616
SimonDebray <debray.c.simon@gmail.com>
2019-07-04 14:24:15 +0200

    card creation done

--- routes/back.js
+++ routes/back.js
@@ -32,6 +32,33 @@ router.get('/questions', function(req, res, next) {
     });
 });
 
+/* GET back office questions. */
+router.post('/questions/new', function(req, res, next) {
+  
+  if (Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  
+  let file = req.files.card,
+    path = '/cards/' + req.files.card.name,
+    direct = './cards/' + req.files.card.name;
+  
+  // Use the mv() method to place the file somewhere on your server
+  file.mv(direct, function(err) {
+    if (err)
+      return res.status(500).send(err);
+  
+    req.body.card = path;
+  
+    let addDoc = db.collection(req.body.country).add(
+      req.body
+    ).then(ref => {
+      console.log('Added document with ID: ', ref.id);
+    });
+  });
+  res.redirect("back/questions")
+});
+
 /* GET back office users. */
 router.get('/users', function(req, res, next) {
   res.render('back-users', { title: 'Mon super back office' });
