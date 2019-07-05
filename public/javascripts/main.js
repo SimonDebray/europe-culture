af7a72108b50600aeec9cde9df1e4bf1c6302c5a
SimonDebray <debray.c.simon@gmail.com>
2019-07-05 10:27:38 +0200

    add round

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,10 @@ let data = {},
   allCards = {},
   currentCards = {},
   selectedCard,
-  gameData = {};
+  gameData = {},
+  selectedResponse,
+  currentResponses = {},
+  currentQuestion;
 
 if (localStorage.getItem("username")) {
   data.username = localStorage.getItem("username");
@@ -46,14 +49,14 @@ socket.on('game.init', (data) => {
   for(var i = 0; i < data.cards.length; i++) {
     data.cards[i].id = 'card-' + i;
     if (i < 6) {
-      document.getElementById("myCards").innerHTML += "<img id='card-"+ i +"' src='' alt=''>";
+      document.getElementById("myCards").innerHTML += "<img id='card-"+ i +"' src='"+ data.cards[i].src +"' alt=''>";
       currentCards['card-' + i] = data.cards[i];
       document.getElementById('card-'+ i).addEventListener('click', selectCard(this));
     }
     
     allCards['card-' + i] = data.cards;
   }
-  createCountdown(8, sendQuestion);
+  createCountdown(8, sendQuestion, "cardSelectionTimer");
 });
 
 function sendQuestion() {
@@ -70,28 +73,18 @@ function sendQuestion() {
   selectedCard = null;
 }
 
-socket.on('game.receiveQuestion', (data) => {
-  console.log('New question receive');
-  
-  console.log(data);
-});
-
-socket.on('game.sendResponse', (data) => {
-  console.log('Game started against ' + data);
-  document.getElementById("theGame").classList.remove("hidden");
-  
-  socket.emit('game.start');
-});
-
-
 function selectCard(card) {
   selectedCard = currentCards[card.id];
   document.getElementById(card.id).classList.add("selected");
 }
 
+function selectResponse(response) {
+  selectedResponse = currentResponses[response.id];
+  document.getElementById(response.id).classList.add("selected");
+}
 
 // Handle countdown
-function createCountdown(time, callback) {
+function createCountdown(time, callback, divId) {
   // Set the date we're counting down to
   var countDownDate = new Date().getTime() + time * 1000;
   
@@ -105,7 +98,7 @@ function createCountdown(time, callback) {
     var distance = countDownDate - now;
     
     // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = (Math.floor((distance % (1000 * 60)) / 1000)).toString();
+    document.getElementById(divId).innerHTML = (Math.floor((distance % (1000 * 60)) / 1000)).toString();
     
     // If the count down is over, write some text
     if (distance < 0) {
@@ -115,3 +108,89 @@ function createCountdown(time, callback) {
     }
   }, 1000);
 }
+
+socket.on('game.receiveQuestion', (data) => {
+  
+  console.log('New question receive');
+  currentQuestion = data.question;
+  
+  document.getElementById("myQuestion").innerHTML = data.question.title;
+  document.getElementById("responses").innerHTML = "";
+  
+  var index = getRndInteger(0, data.question.responses.length - 1);
+  
+  for(var i = 0; i < data.question.responses.length; i++) {
+    if(i === index) {
+      document.getElementById("responses").innerHTML += "<div class='response' id='response-good'>"+ data.question.response +"</div>" ;
+      currentResponses['response-good'] = data.response;
+      
+      document.getElementById('response-good').addEventListener('click', selectResponse(this));
+    }
+    
+    data.responses[i].id = 'response-' + i;
+    document.getElementById("responses").innerHTML += "<div class='response' id='response-"+ i +"'>"+ data.responses[i].title +"</div>" ;
+    currentResponses['response-' + i] = data.responses[i];
+    
+    document.getElementById('response-'+ i).addEventListener('click', selectResponse(this));
+  }
+  
+  createCountdown(10, submitResponse, "responseTimer");
+});
+
+socket.on('game.sendResponse', (data) => {
+  console.log('Game started against ' + data);
+  document.getElementById("theGame").classList.remove("hidden");
+  
+  socket.emit('game.start');
+});
+
+function submitResponse() {
+  if (!selectedResponse) {
+    var keys = Object.keys(currentResponses);
+    selectedResponse = currentResponses[keys[0]];
+  }
+  
+  socket.emit('game.sendResponse', {response: selectedResponse, question: currentQuestion, game: gameData});
+  
+  delete currentCards[selectedCard.id];
+  
+  document.getElementById(selectedCard.id).classList.add("hidden");
+  selectedCard = null;
+}
+
+function responseCut() {
+  submitResponse()
+}
+
+function selectCut() {
+  sendQuestion();
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+socket.on('game.ping', () => {
+  setTimeout(function(){
+    socket.emit('game.ping', gameData);
+  }, 1000);
+});
+
+
+socket.on("game.nextRound", () => {
+  setTimeout(function(){
+    var key = Object.keys(allCards);
+  
+    var id = key[0];
+  
+    document.getElementById("myCards").innerHTML += "<img id='"+ id +"' src='"+ allCards[id].src +"' alt=''>";
+  
+    currentCards[id] = allCards[id];
+  
+    createCountdown(8, sendQuestion, "cardSelectionTimer");
+  }, 5000);
+});
+
+socket.on("game.result", (data) => {
+  document.getElementById("result").innerHTML = data ? "Bonne réponse" : "Mauvaise réponse";
+});
